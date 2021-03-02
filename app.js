9c7267311916b57449a321be15f59a626e4c6dfd
Brian <Brian_M.I.L@hotmail.com>
2021-03-02 16:52:21 -0300

    app actualizado

--- app.js
+++ app.js
@@ -1,13 +1,6 @@
 const { crearArchivo } = require("./helpers/funciones")
 const argv = require("./config/yargs")
 
-console.clear()
-
-// const [, , base] = process.argv;
-// const [, baseN] = base.split("=")
-
-// const base = 2;
-
 crearArchivo(argv.b, argv.l, argv.h)
     .then(msg => console.log(msg))
     .catch(err => console.log(err))
