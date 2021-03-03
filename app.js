ae3e4d3a9804344a6ebb35c66a3098b081ae90eb
Brian <Brian_M.I.L@hotmail.com>
2021-03-03 14:45:18 -0300

    commit

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const { crearArchivo } = require("./helpers/funciones")
 const argv = require("./config/yargs")
 
+// La funcion creatArchivo() llama los datos proporcionados por la consola
+
 crearArchivo(argv.b, argv.l, argv.h)
     .then(msg => console.log(msg))
     .catch(err => console.log(err))
